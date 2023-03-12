90b9d04d93bba8a644ad6a9c9ca3320ef1ac91dc
Molodoy3 <artem.nikonov.2005@bk.ru>
2023-03-12 18:44:37 +0500

    plugged in another plugin

--- src/js/app.js
+++ src/js/app.js
@@ -7,11 +7,12 @@ import "./modules/lightgallery.min.js"
 import { isMobile } from './modules/functions.js';
 import { formValidate } from './modules/formValidate.js';
 import { animOnScroll } from './modules/animOnScroll.js';
-import gsap from 'gsap';
+import "./modules/smoothScroll.js";
+/* import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
 gsap.registerPlugin(ScrollTrigger);
 import { ScrollSmoother } from 'gsap/ScrollSmoother.js';
-gsap.registerPlugin(ScrollSmoother);
+gsap.registerPlugin(ScrollSmoother); */
 
 /* import { MousePRLX } from "./modules/parallax-mouse.js"; */
 window.addEventListener("load", windowLoad);
@@ -33,7 +34,7 @@ function windowLoad() {
         window.addEventListener('resize', (e) => {
             if (window.innerWidth > 479.98 && window.innerWidth < 992.98) {
                 buttonMainBlock.style.cssText = `flex: 0 0 ${(window.innerWidth * 0.354) - 10}px`;
-            } else{
+            } else {
                 buttonMainBlock.style.cssText = `flex: 1 1 auto`;
             }
         });
@@ -46,13 +47,26 @@ function windowLoad() {
     window.addEventListener('scroll', () => {
 
     });
-    //Создание плавного скролла
-    ScrollSmoother.create({
+    /*     ScrollSmoother.create({
         wrapper: '.wrapper',
         content: '.content',
         smooth: 1.4,
         effects: true,
+    }); */
+    //Создание плавного скролла
+    SmoothScroll({
+        animationTime: 600,
+        stepSize: 75,
+        accelerationDelta: 30,
+        accelerationMax: 2,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        pulseAlgorithm: true,
+        pulseScale: 4,
+        pulseNormalize: 1,
+        touchpadSupport: true,
     });
+
     //Инициализация слайдера
     function initSliders() {
         const slider = document.querySelector('.slider-main-block');
